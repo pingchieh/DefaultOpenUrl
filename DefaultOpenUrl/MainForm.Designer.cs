6fe8cf5751f37910707c487b3efee922148edece
Chieh <li.pingchieh@outlook.com>
2024-08-28 19:55:52 +0800

    添加项目文件。

---
+++ DefaultOpenUrl/MainForm.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace DefaultOpenUrl
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if(disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.btn_SetOpenUrl = new System.Windows.Forms.Button();
+            this.btn_CopyUrl = new System.Windows.Forms.Button();
+            this.btn_OpenInEdge = new System.Windows.Forms.Button();
+            this.btn_OpenInEdgeInPrivate = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btn_SetOpenUrl
+            // 
+            this.btn_SetOpenUrl.Location = new System.Drawing.Point(26, 45);
+            this.btn_SetOpenUrl.Name = "btn_SetOpenUrl";
+            this.btn_SetOpenUrl.Size = new System.Drawing.Size(92, 37);
+            this.btn_SetOpenUrl.TabIndex = 0;
+            this.btn_SetOpenUrl.Text = "注册程序";
+            this.btn_SetOpenUrl.UseVisualStyleBackColor = true;
+            this.btn_SetOpenUrl.Click += new System.EventHandler(this.btn_SetOpenUrl_Click);
+            // 
+            // btn_CopyUrl
+            // 
+            this.btn_CopyUrl.Enabled = false;
+            this.btn_CopyUrl.Location = new System.Drawing.Point(124, 45);
+            this.btn_CopyUrl.Name = "btn_CopyUrl";
+            this.btn_CopyUrl.Size = new System.Drawing.Size(92, 37);
+            this.btn_CopyUrl.TabIndex = 1;
+            this.btn_CopyUrl.Text = "复制链接";
+            this.btn_CopyUrl.UseVisualStyleBackColor = true;
+            this.btn_CopyUrl.Click += new System.EventHandler(this.btn_CopyUrl_Click);
+            // 
+            // btn_OpenInEdge
+            // 
+            this.btn_OpenInEdge.Enabled = false;
+            this.btn_OpenInEdge.Location = new System.Drawing.Point(222, 45);
+            this.btn_OpenInEdge.Name = "btn_OpenInEdge";
+            this.btn_OpenInEdge.Size = new System.Drawing.Size(92, 37);
+            this.btn_OpenInEdge.TabIndex = 2;
+            this.btn_OpenInEdge.Text = "使用Edge打开";
+            this.btn_OpenInEdge.UseVisualStyleBackColor = true;
+            this.btn_OpenInEdge.Click += new System.EventHandler(this.btn_OpenInEdge_Click);
+            // 
+            // btn_OpenInEdgeInPrivate
+            // 
+            this.btn_OpenInEdgeInPrivate.Enabled = false;
+            this.btn_OpenInEdgeInPrivate.Location = new System.Drawing.Point(320, 45);
+            this.btn_OpenInEdgeInPrivate.Name = "btn_OpenInEdgeInPrivate";
+            this.btn_OpenInEdgeInPrivate.Size = new System.Drawing.Size(92, 37);
+            this.btn_OpenInEdgeInPrivate.TabIndex = 3;
+            this.btn_OpenInEdgeInPrivate.Text = "Edge无痕模式";
+            this.btn_OpenInEdgeInPrivate.UseVisualStyleBackColor = true;
+            this.btn_OpenInEdgeInPrivate.Click += new System.EventHandler(this.btn_OpenInEdgeInPrivate_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(440, 120);
+            this.Controls.Add(this.btn_OpenInEdgeInPrivate);
+            this.Controls.Add(this.btn_OpenInEdge);
+            this.Controls.Add(this.btn_CopyUrl);
+            this.Controls.Add(this.btn_SetOpenUrl);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "DefaultOpenUrl";
+            this.TopMost = true;
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_SetOpenUrl;
+        private System.Windows.Forms.Button btn_CopyUrl;
+        private System.Windows.Forms.Button btn_OpenInEdge;
+        private System.Windows.Forms.Button btn_OpenInEdgeInPrivate;
+    }
+}
+
---
+++ DefaultOpenUrl/MainForm.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.Win32;
+
+namespace DefaultOpenUrl
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        string url = "";
+
+        public MainForm(string url)
+        {
+            this.url = url;
+            InitializeComponent();
+            if (url != "")
+            {
+                btn_CopyUrl.Enabled = true;
+                btn_OpenInEdge.Enabled = true;
+                btn_OpenInEdgeInPrivate.Enabled = true;
+                btn_SetOpenUrl.Enabled = false;
+            }
+        }
+
+        private void btn_SetOpenUrl_Click(object sender, EventArgs e)
+        {
+            // 自动获取程序的名称和路径
+            string appName = Assembly.GetExecutingAssembly().GetName().Name;
+            string appPath = Assembly.GetExecutingAssembly().Location;
+
+            // 注册 http 协议
+            using (
+                RegistryKey httpKey = Registry.CurrentUser.CreateSubKey(
+                    $@"SOFTWARE\Classes\{appName}.http"
+                )
+            )
+            {
+                httpKey.SetValue("", "URL:HTTP");
+                httpKey.SetValue("URL Protocol", "");
+                using (RegistryKey commandKey = httpKey.CreateSubKey(@"shell\open\command"))
+                {
+                    commandKey.SetValue("", $"\"{appPath}\" \"%1\"");
+                }
+            }
+
+            // 注册 https 协议
+            using (
+                RegistryKey httpsKey = Registry.CurrentUser.CreateSubKey(
+                    $@"SOFTWARE\Classes\{appName}.https"
+                )
+            )
+            {
+                httpsKey.SetValue("", "URL:HTTPS");
+                httpsKey.SetValue("URL Protocol", "");
+                using (RegistryKey commandKey = httpsKey.CreateSubKey(@"shell\open\command"))
+                {
+                    commandKey.SetValue("", $"\"{appPath}\" \"%1\"");
+                }
+            }
+
+            // 注册程序的功能和协议关联
+            using (
+                RegistryKey capabilitiesKey = Registry.CurrentUser.CreateSubKey(
+                    $@"SOFTWARE\Classes\{appName}\Capabilities"
+                )
+            )
+            {
+                capabilitiesKey.SetValue("ApplicationName", appName);
+                capabilitiesKey.SetValue("ApplicationIcon", $"{appPath},0");
+                capabilitiesKey.SetValue("ApplicationDescription", $"{appName} - Custom Browser");
+
+                using (RegistryKey urlAssocKey = capabilitiesKey.CreateSubKey("UrlAssociations"))
+                {
+                    urlAssocKey.SetValue("http", $"{appName}.http");
+                    urlAssocKey.SetValue("https", $"{appName}.https");
+                }
+            }
+
+            // 注册到 RegisteredApplications
+            using (
+                RegistryKey regAppsKey = Registry.CurrentUser.CreateSubKey(
+                    @"SOFTWARE\RegisteredApplications"
+                )
+            )
+            {
+                regAppsKey.SetValue(appName, $@"SOFTWARE\Classes\{appName}\Capabilities");
+            }
+        }
+
+        private void btn_CopyUrl_Click(object sender, EventArgs e)
+        {
+            if (url != "")
+            {
+                Clipboard.SetText(url);
+                MessageBox.Show("URL已复制到剪贴板: " + url);
+            }
+            else
+            {
+                MessageBox.Show("URL为空");
+            }
+        }
+
+        private void btn_OpenInEdge_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // 使用Microsoft Edge打开URL
+                System.Diagnostics.Process.Start("msedge.exe", url);
+                Console.WriteLine("已使用Edge打开URL。");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"打开Edge时出错: {ex.Message}");
+            }
+        }
+
+        private void btn_OpenInEdgeInPrivate_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // 使用Microsoft Edge隐私模式打开URL
+                System.Diagnostics.Process.Start("msedge.exe", $"--inprivate {url}");
+                Console.WriteLine("已使用Edge隐私模式打开URL。");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"打开Edge时出错: {ex.Message}");
+            }
+        }
+    }
+}
