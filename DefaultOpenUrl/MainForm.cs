01b28783ba31b61ae3642c4a01824ef4ae584ff2
pingchieh <20982498+pingchieh@users.noreply.github.com>
2024-08-30 00:07:44 +0800

    重构代码并改进字符串处理
    将 `url` 字符串声明为 `readonly` 字段，并在构造函数中初始化。将 `url` 的空字符串检查替换为 `string.IsNullOrEmpty` 方法。重构处理按钮点击事件的代码为多个方法：`RegisterProtocol`、`RegisterApplicationCapabilities` 和 `RegisterApplicationInRegistry`。更新注释和文档字符串，使其更加清晰和详细。在 `Btn_CopyUrl_Click`、`Btn_OpenInEdge_Click` 和 `Btn_OpenInEdgeInPrivate_Click` 方法中，使用字符串插值替换字符串连接，并使用 `Close()` 方法替换 `this.Close()`。

--- DefaultOpenUrl/MainForm.cs
+++ DefaultOpenUrl/MainForm.cs
@@ -1,12 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
 using System.Reflection;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 using Microsoft.Win32;
 
@@ -14,18 +7,18 @@ namespace DefaultOpenUrl
 {
     public partial class MainForm : Form
     {
+        private readonly string url;
+
         public MainForm()
         {
             InitializeComponent();
         }
 
-        string url = "";
-
         public MainForm(string url)
             : this()
         {
             this.url = url;
-            if (url != "")
+            if (!string.IsNullOrEmpty(url))
             {
                 btn_CopyUrl.Enabled = true;
                 btn_OpenInEdge.Enabled = true;
@@ -35,47 +28,52 @@ namespace DefaultOpenUrl
         }
 
         /// <summary>
-        /// 处理按钮点击事件，注册应用程序的 http 和 https 协议，并将其与应用程序功能关联。
+        /// 当按钮设置打开URL被点击时，注册应用程序的协议、能力和注册表项。
         /// </summary>
-        /// <param name="sender">触发事件的对象</param>
-        /// <param name="e">包含事件数据的 EventArgs 对象</param>
+        /// <param name="sender">触发事件的对象。</param>
+        /// <param name="e">包含事件数据的 EventArgs 对象。</param>
         private void Btn_SetOpenUrl_Click(object sender, EventArgs e)
         {
-            // 自动获取程序的名称和路径
             string appName = Assembly.GetExecutingAssembly().GetName().Name;
             string appPath = Assembly.GetExecutingAssembly().Location;
 
-            // 注册 http 协议
+            RegisterProtocol(appName, appPath, "http");
+            RegisterProtocol(appName, appPath, "https");
+
+            RegisterApplicationCapabilities(appName, appPath);
+            RegisterApplicationInRegistry(appName);
+        }
+
+        /// <summary>
+        /// 在注册表中为指定的应用程序注册特定协议。
+        /// </summary>
+        /// <param name="appName">应用程序名称。</param>
+        /// <param name="appPath">应用程序路径。</param>
+        /// <param name="protocol">要注册的协议。</param>
+        private void RegisterProtocol(string appName, string appPath, string protocol)
+        {
             using (
-                RegistryKey httpKey = Registry.CurrentUser.CreateSubKey(
-                    $@"SOFTWARE\Classes\{appName}.http"
+                RegistryKey protocolKey = Registry.CurrentUser.CreateSubKey(
+                    $@"SOFTWARE\Classes\{appName}.{protocol}"
                 )
             )
             {
-                httpKey.SetValue("", "URL:HTTP");
-                httpKey.SetValue("URL Protocol", "");
-                using (RegistryKey commandKey = httpKey.CreateSubKey(@"shell\open\command"))
+                protocolKey.SetValue("", $"URL:{protocol.ToUpper()}");
+                protocolKey.SetValue("URL Protocol", "");
+                using (RegistryKey commandKey = protocolKey.CreateSubKey(@"shell\open\command"))
                 {
                     commandKey.SetValue("", $"\"{appPath}\" \"%1\"");
                 }
             }
+        }
 
-            // 注册 https 协议
-            using (
-                RegistryKey httpsKey = Registry.CurrentUser.CreateSubKey(
-                    $@"SOFTWARE\Classes\{appName}.https"
-                )
-            )
-            {
-                httpsKey.SetValue("", "URL:HTTPS");
-                httpsKey.SetValue("URL Protocol", "");
-                using (RegistryKey commandKey = httpsKey.CreateSubKey(@"shell\open\command"))
-                {
-                    commandKey.SetValue("", $"\"{appPath}\" \"%1\"");
-                }
-            }
-
-            // 注册程序的功能和协议关联
+        /// <summary>
+        /// 注册应用程序的功能，包括设置应用程序的名称、图标、描述和URL关联。
+        /// </summary>
+        /// <param name="appName">应用程序的名称。</param>
+        /// <param name="appPath">应用程序的路径。</param>
+        private void RegisterApplicationCapabilities(string appName, string appPath)
+        {
             using (
                 RegistryKey capabilitiesKey = Registry.CurrentUser.CreateSubKey(
                     $@"SOFTWARE\Classes\{appName}\Capabilities"
@@ -85,15 +83,20 @@ namespace DefaultOpenUrl
                 capabilitiesKey.SetValue("ApplicationName", appName);
                 capabilitiesKey.SetValue("ApplicationIcon", $"{appPath},0");
                 capabilitiesKey.SetValue("ApplicationDescription", $"{appName} - Custom Browser");
-
                 using (RegistryKey urlAssocKey = capabilitiesKey.CreateSubKey("UrlAssociations"))
                 {
                     urlAssocKey.SetValue("http", $"{appName}.http");
                     urlAssocKey.SetValue("https", $"{appName}.https");
                 }
             }
+        }
 
-            // 注册到 RegisteredApplications
+        /// <summary>
+        /// 在注册表中注册应用程序。
+        /// </summary>
+        /// <param name="appName">应用程序的名称。</param>
+        private void RegisterApplicationInRegistry(string appName)
+        {
             using (
                 RegistryKey regAppsKey = Registry.CurrentUser.CreateSubKey(
                     @"SOFTWARE\RegisteredApplications"
@@ -105,21 +108,23 @@ namespace DefaultOpenUrl
         }
 
         /// <summary>
-        /// 当点击"CopyUrl"按钮时处理的事件。如果URL不为空，将其复制到剪贴板并显示一条消息。如果URL为空，则显示一条错误消息。
+        /// 当点击复制URL按钮时触发的事件处理程序。
+        /// 如果URL不为空，将其复制到剪贴板并询问是否关闭程序。
+        /// 如果URL为空，则显示一条消息提示URL为空。
         /// </summary>
         /// <param name="sender">触发事件的对象。</param>
         /// <param name="e">包含事件数据的EventArgs对象。</param>
         private void Btn_CopyUrl_Click(object sender, EventArgs e)
         {
-            if (url != "")
+            if (!string.IsNullOrEmpty(url))
             {
                 Clipboard.SetText(url);
                 if (
-                    MessageBox.Show("URL已复制到剪贴板,是否关闭程序?\n " + url, "提示", MessageBoxButtons.YesNo)
+                    MessageBox.Show($"URL已复制到剪贴板,是否关闭程序?\n {url}", "提示", MessageBoxButtons.YesNo)
                     == DialogResult.Yes
                 )
                 {
-                    this.Close();
+                    Close();
                 }
             }
             else
@@ -129,39 +134,39 @@ namespace DefaultOpenUrl
         }
 
         /// <summary>
-        /// 处理按钮点击事件，使用Microsoft Edge打开指定的URL。
+        /// 处理按钮点击事件，询问用户是否在Microsoft Edge中打开URL。
         /// </summary>
         /// <param name="sender">触发事件的对象。</param>
-        /// <param name="e">包含事件数据的EventArgs对象。</param>
+        /// <param name="e">包含事件数据的EventArgs。</param>
         private void Btn_OpenInEdge_Click(object sender, EventArgs e)
         {
             if (
-                MessageBox.Show("是否要在Microsoft Edge中打开URL?\n" + url, "提示", MessageBoxButtons.YesNo)
+                MessageBox.Show($"是否要在Microsoft Edge中打开URL?\n{url}", "提示", MessageBoxButtons.YesNo)
                 == DialogResult.No
             )
             {
                 return;
             }
             System.Diagnostics.Process.Start("msedge.exe", url);
-            this.Close();
+            Close();
         }
 
         /// <summary>
-        /// 在Microsoft Edge的隐私模式下打开URL的事件处理程序。
+        /// 当用户点击按钮时，在隐私模式下使用Edge浏览器打开指定的URL。
         /// </summary>
         /// <param name="sender">触发事件的对象。</param>
         /// <param name="e">包含事件数据的EventArgs对象。</param>
         private void Btn_OpenInEdgeInPrivate_Click(object sender, EventArgs e)
         {
             if (
-                MessageBox.Show("是否要在隐私模式下打开URL?\n" + url, "提示", MessageBoxButtons.YesNo)
+                MessageBox.Show($"是否要在隐私模式下打开URL?\n{url}", "提示", MessageBoxButtons.YesNo)
                 == DialogResult.No
             )
             {
                 return;
             }
             System.Diagnostics.Process.Start("msedge.exe", $"--inprivate {url}");
-            this.Close();
+            Close();
         }
     }
 }
